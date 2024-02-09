c2caaeca68463899230740fd06ed78a5c9f42dcd
umizoomi <you@example.com>
2024-02-09 15:10:50 -0500

    figured out round/counter system

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,41 @@
-class Conversation {
+import java.util.Scanner;
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
-  }
+//define class
+public class Conversation {
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+        
+        //print first message
+        System.out.println("Hi there. I'm Uma. How many rounds would you like to play? ");
+        int rounds = sc.nextInt();
+        
+        //clear input
+        sc.nextLine();
+        //ask user for input
+        System.out.println("Okay. what would u like to say");
+        
+        int counter = 0;
+
+        while (counter < rounds) {
+            counter = counter + 1;
+            // Get user's statement / input
+            String userInput = sc.nextLine(); 
+
+            // Respond to question
+            if (userInput.contains("?")) {
+                System.out.println("How interesting...");
+            }
+            // Respond to statement
+            else {
+                System.out.println(userInput + "?");
+            }
+
+
+        }
+        System.out.println("Goodbye!");
+        sc.close();
+
+    }
 }
