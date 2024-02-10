f177a46200d64963599689434e7d86afc85b8b1d
umizoomi <you@example.com>
2024-02-10 14:23:17 -0500

    worked on mirroring  conditionals

--- Conversation.java
+++ Conversation.java
@@ -14,11 +14,11 @@ public class Conversation {
         //clear input
         sc.nextLine();
         //ask user for input
-        System.out.println("Okay. what would u like to say");
+        System.out.println("Okay. What's on your mind?");
         
         int counter = 0;
 
-        while (counter < rounds) {
+        while (counter < rounds) { //while the conversation is still going:
             counter = counter + 1;
             // Get user's statement / input
             String userInput = sc.nextLine(); 
@@ -28,9 +28,31 @@ public class Conversation {
                 System.out.println("How interesting...");
             }
             // Respond to statement
-            else {
-                System.out.println(userInput + "?");
+            else { //monster if statements
+              //put this in a function called mirror? that way you can use it for questions too
+              if (userInput.contains("I")) {
+                System.out.println(userInput.replace("I", "You") + "?");
             }
+              if (userInput.contains("me")) {
+                System.out.println(userInput.replace("me", "you") + "?");
+            }
+              if (userInput.contains("am")) {
+                System.out.println(userInput.replace("am", "are") + "?");
+            }
+              if (userInput.contains("you")) {
+                System.out.println(userInput.replace("am", "are") + "?");
+            }
+              if (userInput.contains("my")) {
+                System.out.println(userInput.replace("my", "your") + "?");
+            }
+              if (userInput.contains("your")) {
+                System.out.println(userInput.replace("your", "my") + "?");
+            }
+              else {
+                System.out.println("Oh...seek help maybe."); //add random choice from list here
+            }
+            
+          }
 
 
         }
